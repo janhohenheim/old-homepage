e7d2af4d4d13ccdb2dd0df5f3f73e133ff43cf14
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-02-22 09:38:15 +0100

    Create Event Loop

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,53 @@ extern crate chat;
 extern crate mio;
 
 use mio::*;
+use mio::tcp::{TcpListener, TcpStream};
 
 fn main() {
+    // Setup some tokens to allow us to identify which event is
+    // for which socket.
+    const SERVER: Token = Token(0);
+    const CLIENT: Token = Token(1);
 
+    let addr = "127.0.0.1:13265".parse().unwrap();
+
+    // Setup the server socket
+    let server = TcpListener::bind(&addr).unwrap();
+
+    // Create an poll instance
+    let poll = Poll::new().unwrap();
+
+    // Start listening for incoming connections
+    poll.register(&server, SERVER, Ready::readable(),
+                  PollOpt::edge()).unwrap();
+
+    // Setup the client socket
+    let sock = TcpStream::connect(&addr).unwrap();
+
+    // Register the socket
+    poll.register(&sock, CLIENT, Ready::readable(),
+                  PollOpt::edge()).unwrap();
+
+    // Create storage for events
+    let mut events = Events::with_capacity(1024);
+
+    loop {
+        poll.poll(&mut events, None).unwrap();
+
+        for event in events.iter() {
+            match event.token() {
+                SERVER => {
+                    // Accept and drop the socket immediately, this will close
+                    // the socket and notify the client of the EOF.
+                    let _ = server.accept();
+                }
+                CLIENT => {
+                    // The server just shuts down the socket, let's just exit
+                    // from our event loop.
+                    return;
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
 }
