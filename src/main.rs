b890d1540115765c54af82093cad8c6a5a605d5c
SirRade <jn_ferner@hotmail.de>
2017-03-03 00:28:33 +0100

    Deus est mortus, logica optinet

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,18 @@ extern crate hyper_native_tls;
 
 use iron::mime::{Mime, TopLevel, SubLevel};
 use iron::headers::ContentType;
+use iron::{Iron, Request, Response, method};
+use iron::status;
+use std::result::Result;
 
 fn main() {
-    // Avoid unused errors due to conditional compilation ('native-tls-example' feature is not default)
-    use hyper_native_tls::NativeTlsServer;
-    use iron::{Iron, Request, Response};
-    use iron::status;
-    use std::result::Result;
-
-    let ssl = NativeTlsServer::new("identity.p12", "mypass").unwrap();
-
-    match Iron::new(|_: &mut Request| {
+    match Iron::new(|request: &mut Request| {
         println!("Got an incoming connection!");
-        let mut response = Response::with((status::Ok, "<h1>Hello world!</h1>"));
+        let mut response = Response::with((status::Ok, "<h1>Hello world!</h1><br />:)"));
         response.headers.set(ContentType(Mime(TopLevel::Text, SubLevel::Html, vec![])));
         Ok(response)
-    }).https("127.0.0.1:3000", ssl) {
+    }).http("127.0.0.1:8080") {
         Result::Ok(listening) => println!("{:?}", listening),
         Result::Err(err) => panic!("{:?}", err),
     }
-    // curl -vvvv https://127.0.0.1:3000/ -k
 }
