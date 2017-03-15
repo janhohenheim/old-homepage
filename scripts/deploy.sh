cf1cd7d3b72041b6039362c8d24b04528954fb8b
SirRade <jn_ferner@hotmail.de>
2017-03-15 22:13:08 +0100

    Make deploy use latest version of our package

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 git pull
+rm -f rpm/millionaire-*.rpm
 ./scripts/package.sh
-scp rpm/x86_64/millionaire-0.3.0-1.x86_64.rpm jnferner.com:~/
-ssh jnferner.com rpm -Uhv millionaire-0.3.0-1.x86_64.rpm
+ssh jnferner.com rm -f millionaire-*.rpm
+scp rpm/x86_64/millionaire-*.rpm jnferner.com:~/
+ssh jnferner.com rpm -Uhv millionaire-*.rpm
 
