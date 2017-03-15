b7a6a033f2fb711160f04d62a168227334a713b0
SirRade <jn_ferner@hotmail.de>
2017-03-15 22:20:36 +0100

    Update paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 git pull
-rm -f rpm/millionaire-*.rpm
+rm -f rpm/x86_64/millionaire-*.rpm
 ./scripts/package.sh
-ssh jnferner.com rm -f millionaire-*.rpm
+ssh jnferner.com rm -f 'millionaire-*.rpm'
 scp rpm/x86_64/millionaire-*.rpm jnferner.com:~/
-ssh jnferner.com rpm -Uhv millionaire-*.rpm
+ssh jnferner.com rpm -Uhv 'millionaire-*.rpm'
 
