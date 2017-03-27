d46c0fecd546d66609d40a944ce2b0253f92c7cc
SirRade <jn_ferner@hotmail.de>
2017-03-27 21:50:28 +0200

    Add some diesel into the fire

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
-#[macro_use]
-extern crate router;
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate router;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate diesel_codegen;
+
 pub mod routing;
 pub mod templating;
 pub mod session;
