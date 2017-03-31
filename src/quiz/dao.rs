8da50f9488f4d2e45c3b7a97b9ff2c6768853e1d
SirRade <jn_ferner@hotmail.de>
2017-03-31 11:39:53 +0200

    Run clippy

--- src/quiz/dao.rs
+++ src/quiz/dao.rs
@@ -3,7 +3,6 @@ extern crate diesel;
 
 use self::dotenv::dotenv;
 use std::env;
-use super::model::Player;
 use self::diesel::prelude::*;
 use self::diesel::pg::PgConnection;
 
@@ -12,14 +11,4 @@ pub fn establish_connection() -> PgConnection {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
-
-pub fn generate_player_id() -> u32 {
-    unimplemented!();
-    0
-}
-
-pub fn get_player(id: u32) -> Player {
-    unimplemented!();
-    Player {}
 }
