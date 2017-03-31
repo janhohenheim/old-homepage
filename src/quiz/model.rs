0dbdd08819ac29a1f4f67ebf5ad5ae80a3b00392
SirRade <jn_ferner@hotmail.de>
2017-03-31 14:11:45 +0200

    Untangle the HasMany mess

--- src/quiz/model.rs
+++ src/quiz/model.rs
@@ -1,36 +1,33 @@
 use super::schema::*;
 
-#[derive(Queryable, Identifiable)]
+#[table_name="category"]
+#[derive(Queryable, Identifiable, Associations, Debug)]
+#[has_many(question)]
+pub struct Category {
+        pub id: i32,
+        pub text: String,
+}
+
+
+#[table_name="question"]
+#[derive(Queryable, Identifiable, Associations)]
+#[has_many(answer)]
+#[belongs_to(Category)]
+pub struct Question {
+    pub id: i32,
+    pub category_id: i32,
+    pub text: String
+}
+
 #[table_name="answer"]
+#[derive(Queryable, Identifiable, Associations)]
+#[belongs_to(Question)]
 pub struct Answer {
     pub id: i32,
     pub question_id: i32,
     pub text: String,
     pub is_correct: bool,
 }
-
-HasMany! {
-    (answer, foreign_key = question_id)
-    #[table_name(question)]
-    #[derive(Queryable, Identifiable)]
-    pub struct Question {
-        pub id: i32,
-        pub category_id: i32,
-        pub text: String,
-        pub answers: Vec<Answer>,
-    }
-}
-
-HasMany! {
-    (question, foreign_key = category_id)
-    #[table_name(category)]
-    #[derive(Queryable, Identifiable)]
-    pub struct Category {
-        pub id: i32,
-        pub text: String,
-    }
-}
-
 
 
 pub struct Player {}
