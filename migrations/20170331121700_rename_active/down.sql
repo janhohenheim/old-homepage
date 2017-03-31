cf4fc2080e3665876a40a90e03f857538ad57406
SirRade <jn_ferner@hotmail.de>
2017-03-31 14:21:44 +0200

    DB: Rename active to is_active

---
+++ migrations/20170331121700_rename_active/down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE category RENAME COLUMN is_active TO active;
+ALTER TABLE question RENAME COLUMN is_active TO active;
+ALTER TABLE answer RENAME COLUMN is_active TO active;
---
+++ migrations/20170331121700_rename_active/up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE category RENAME COLUMN active TO is_active;
+ALTER TABLE question RENAME COLUMN active TO is_active;
+ALTER TABLE answer RENAME COLUMN active TO is_active;
