fb7cbd4fdfc6ea2a6433f8fd4c87bc970a900516
SirRade <jn_ferner@hotmail.de>
2017-03-31 16:55:05 +0200

    Do whitespace lol

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,6 @@ fn main() {
     let mut chain = routing::create_chain();
     templating::link_to_chain(&mut chain).unwrap();
     session::link_to_chain(&mut chain).unwrap();
-
+    
     Iron::new(chain).http("localhost:8080").unwrap();
 }
